85f37e13acbd528c1907e0eb8e27af84f4c9cfad
FlashCodx <47036505+FlashCodX@users.noreply.github.com>
2020-10-06 17:13:47 +0100

    Updates

---
+++ precache-manifest.eed0dfc2c4331e052ed2e07d0b4ef6c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472b11463a099c0a6466afff266248f0",
+    "url": "/tv-universe/index.html"
+  },
+  {
+    "revision": "eca157897ef9cd38e2ec",
+    "url": "/tv-universe/static/css/main.e1151856.chunk.css"
+  },
+  {
+    "revision": "1b49a3bcc22654a49d3f",
+    "url": "/tv-universe/static/js/2.8e63bc45.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tv-universe/static/js/2.8e63bc45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eca157897ef9cd38e2ec",
+    "url": "/tv-universe/static/js/main.80e68a3e.chunk.js"
+  },
+  {
+    "revision": "63ec4461cefa11506e92",
+    "url": "/tv-universe/static/js/runtime-main.ace647ca.js"
+  }
+]);
